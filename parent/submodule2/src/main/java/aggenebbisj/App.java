6f9269cdf672eee055c2835b3faba40b97c70f6a
unknown <rjo19873@BA91-CNU320C59X.gb.tntpost.com>
2013-07-22 23:09:06 +0200

    initial commit

---
+++ parent/submodule2/src/main/java/aggenebbisj/App.java
@@ -0,0 +1,13 @@
+package aggenebbisj;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
